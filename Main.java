f79c0b684da9cc990dff1781e8fef81a926203a6
tsetse0 <80219531+tsetse0@users.noreply.github.com>
2021-06-08 14:07:43 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,37 @@
 
 public class Main
 {
-    // μεταβλητές στιγμιοτύπου - αντικαταστήστε το ακόλουθο παράδειγμα
-    // με τις δικές σας μεταβλητές
-    
-    public static void main(String[] args) {
-      new StartGame();
-   }
 
-   
-}
+    public static void main(String[] args) {
+        //Set buff elements
+        
+        BuffElement b = new BuffElement("Golden Sword", "Damage", 5, 5);
+        CatalogBuff.addBuff(b);
+        
+        b = new BuffElement("Silver Shield", "HP", 5, 5);
+        CatalogBuff.addBuff(b);
+        
+        b = new BuffElement("Sneaky Arrow", "Critical", 5, 5);
+        CatalogBuff.addBuff(b);
+        
+        b = new BuffElement("Hammer of Thor", "Damage", 10, 10);
+        CatalogBuff.addBuff(b);
+        
+        b = new BuffElement("Black Castle", "HP", 10, 10);
+        CatalogBuff.addBuff(b);
+        
+        b = new BuffElement("Master Of Target", "Critical", 10, 10);
+        CatalogBuff.addBuff(b);
+        
+        b = new BuffElement("Killer Mode", "Damage", 15, 15);
+        CatalogBuff.addBuff(b);
+        
+        b = new BuffElement("Tank Mode", "HP", 15, 15);
+        CatalogBuff.addBuff(b);
+        
+        b = new BuffElement("Shooter Mode", "Critical", 15, 15);
+        CatalogBuff.addBuff(b);
+        
+        //Start game
+        new StartGame();
+   }
